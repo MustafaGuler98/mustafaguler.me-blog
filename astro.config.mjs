e62e28a85ee3b0205dd8b525192b68009f831b26
Mustafa Güler <mail.mustafaguler@gmail.com>
2025-10-01 17:17:22 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://mustafaguler.me',
+  base: 'blog',
+  i18n: {
+    locales: ['tr', 'en'],
+    defaultLocale: 'tr',
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true
+    }
+  }
+});
